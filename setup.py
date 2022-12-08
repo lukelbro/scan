eb2d419c6dadfd469d65667368f2a11b5eb7297d
Luke Brown <53307399+lukelbro@users.noreply.github.com>
2022-12-08 17:47:47 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="e11scan",
-    version="0.1",
+    version="0.1.1",
     author="Luke Brown",
     packages=find_packages(exclude=['*tests']),
     install_requires=['h5py', 'pandas', 'scipy', 'numpy', 'colorama', 'matplotlib']
